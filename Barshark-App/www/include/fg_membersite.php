dd44247c73fc7e1c753ab04f24d727cecb735295
viciousveritas <markybarra@Marks-MacBook-Pro.local>
2016-06-24 21:15:26 -0400

    suck
    me

--- Barshark-App/www/include/fg_membersite.php
+++ Barshark-App/www/include/fg_membersite.php
@@ -886,7 +886,7 @@ class FGMembersite
             ,    '<div class="contact_info">'
             ,    'Hours: <span>' . $row["hours"] . '</span> <br />'
             ,    'Phone: ' . $row["phone"] . ' <br />'
-            ,    'Menu: <a href="' . $row["menu"] . '" target="_blank">OPEN</a>'
+            ,    'Menu: <a href="' . $row["menu"] . '" class="external" target="_blank">OPEN</a>'
             ,    '</div>'
             ,    '<h3>Our Location</h3>'
             ,    '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3596.6251316836215!2d-80.34114568504981!3d25.65056428368738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9c6ecbeb5721f%3A0x669152096eaa540e!2s8926+SW+129th+St%2C+Miami%2C+FL+33176!5e0!3m2!1sen!2sus!4v1452033011970" width="100%" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>'
